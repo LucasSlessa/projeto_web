42f81b00d048392ee475a5c55090a89fa05e5c22
Lucas Lessa <lessalucas1102@gmail.com>
2024-05-23 19:43:15 -0300

    v2

--- course_db.sql
+++ course_db.sql
@@ -1,72 +1,56 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Sep 30, 2022 at 05:35 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           8.2.0 - MySQL Community Server - GPL
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              12.0.0.6468
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Database: `course_db`
---
 
--- --------------------------------------------------------
+-- Copiando estrutura do banco de dados para course_db
+CREATE DATABASE IF NOT EXISTS `course_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `course_db`;
 
---
--- Table structure for table `bookmark`
---
-
-CREATE TABLE `bookmark` (
+-- Copiando estrutura para tabela course_db.bookmark
+CREATE TABLE IF NOT EXISTS `bookmark` (
   `user_id` varchar(20) NOT NULL,
   `playlist_id` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
+-- Copiando dados para a tabela course_db.bookmark: ~0 rows (aproximadamente)
 
---
--- Table structure for table `comments`
---
-
-CREATE TABLE `comments` (
+-- Copiando estrutura para tabela course_db.comments
+CREATE TABLE IF NOT EXISTS `comments` (
   `id` varchar(20) NOT NULL,
   `content_id` varchar(20) NOT NULL,
   `user_id` varchar(20) NOT NULL,
   `tutor_id` varchar(20) NOT NULL,
   `comment` varchar(1000) NOT NULL,
-  `date` date NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
+-- Copiando dados para a tabela course_db.comments: ~1 rows (aproximadamente)
 
---
--- Table structure for table `contact`
---
-
-CREATE TABLE `contact` (
+-- Copiando estrutura para tabela course_db.contact
+CREATE TABLE IF NOT EXISTS `contact` (
   `name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `number` int(10) NOT NULL,
+  `number` int NOT NULL,
   `message` varchar(1000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
+-- Copiando dados para a tabela course_db.contact: ~0 rows (aproximadamente)
 
---
--- Table structure for table `content`
---
-
-CREATE TABLE `content` (
+-- Copiando estrutura para tabela course_db.content
+CREATE TABLE IF NOT EXISTS `content` (
   `id` varchar(20) NOT NULL,
   `tutor_id` varchar(20) NOT NULL,
   `playlist_id` varchar(20) NOT NULL,
@@ -74,68 +58,61 @@ CREATE TABLE `content` (
   `description` varchar(1000) NOT NULL,
   `video` varchar(100) NOT NULL,
   `thumb` varchar(100) NOT NULL,
-  `date` date NOT NULL DEFAULT current_timestamp(),
-  `status` varchar(20) NOT NULL DEFAULT 'deactive'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` varchar(20) NOT NULL DEFAULT 'deactive',
+  `project_folder` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `likes`
---
-
-CREATE TABLE `likes` (
+-- Copiando estrutura para tabela course_db.likes
+CREATE TABLE IF NOT EXISTS `likes` (
   `user_id` varchar(20) NOT NULL,
   `tutor_id` varchar(20) NOT NULL,
   `content_id` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
+-- Copiando dados para a tabela course_db.likes: ~1 rows (aproximadamente)
 
---
--- Table structure for table `playlist`
---
-
-CREATE TABLE `playlist` (
+-- Copiando estrutura para tabela course_db.playlist
+CREATE TABLE IF NOT EXISTS `playlist` (
   `id` varchar(20) NOT NULL,
   `tutor_id` varchar(20) NOT NULL,
   `title` varchar(100) NOT NULL,
   `description` varchar(1000) NOT NULL,
   `thumb` varchar(100) NOT NULL,
-  `date` date NOT NULL DEFAULT current_timestamp(),
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `status` varchar(20) NOT NULL DEFAULT 'deactive'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `tutors`
---
-
-CREATE TABLE `tutors` (
+-- Copiando estrutura para tabela course_db.tutors
+CREATE TABLE IF NOT EXISTS `tutors` (
   `id` varchar(20) NOT NULL,
   `name` varchar(50) NOT NULL,
   `profession` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `image` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
+-- Copiando estrutura para tabela course_db.users
+CREATE TABLE IF NOT EXISTS `users` (
   `id` varchar(20) NOT NULL,
   `name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `image` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-COMMIT;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- Copiando estrutura para tabela course_db.users_validation
+CREATE TABLE IF NOT EXISTS `users_validation` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ra` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `nascimento` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `funcao` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `cpf` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
