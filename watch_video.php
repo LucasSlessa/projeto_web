22d8451e43ddb9e9f629fb32db610b2e014ecf29
Lucas Lessa <lessalucas1102@gmail.com>
2024-05-23 22:25:34 -0300

    v2

--- watch_video.php
+++ watch_video.php
@@ -132,7 +132,7 @@ if(isset($_POST['update_now'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>videos</title>
+   <title>Projetosx</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
